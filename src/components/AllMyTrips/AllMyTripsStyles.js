4c6b415a8a56cbf653cbcfb9f2de3f55a26df376
Youssef Ahmed <uosefd@gmail.com>
2021-09-03 19:50:04 +0200

    Made the MyTrips Responsive

--- src/components/AllMyTrips/AllMyTripsStyles.js
+++ src/components/AllMyTrips/AllMyTripsStyles.js
@@ -13,13 +13,13 @@ export const Tabs = styled.div`
     display: flex;
     align-items: center;
     margin-bottom: 10px;
+    flex-wrap: wrap;
 `;
 
 export const Tab = styled.div`
     min-width: 100px;
     height: 25px;
     margin: 0 13px 0 0;
-    padding: 5px 25px;
     border-radius: 12.5px;
     background-color: #f7f7f7;
     color: #000;
@@ -37,11 +37,31 @@ export const Tab = styled.div`
         background-color: #2f1b7a;
         color: #fff;
     }
+
+    @media only screen and (max-width: 767px) {
+        min-width: 80px;
+    }
+
+    @media only screen and (max-width: 528px) {
+        margin-bottom: 10px;
+    }
 `;
 
 export const Trips = styled.div`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-row-gap: 10px;
+
+    @media only screen and (max-width: 991px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media only screen and (max-width: 767px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media only screen and (max-width: 470px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
 `;
 
